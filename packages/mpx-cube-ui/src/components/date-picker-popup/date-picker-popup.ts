394bba41b00ac83816f5ab6e646c0e095a5105a4
QiuShuiBai <1125097005@qq.com>
2025-01-26 11:01:13 +0800

    feat(rn): date-picker-popup

--- packages/mpx-cube-ui/src/components/date-picker-popup/date-picker-popup.ts
+++ packages/mpx-cube-ui/src/components/date-picker-popup/date-picker-popup.ts
@@ -2,6 +2,9 @@ import { createTimePickerPopupComponent } from '../../common/helper/create-compo
 import { datePickerMixin } from '../../common/mixins'
 
 createTimePickerPopupComponent({
+  options: {
+    multipleSlots: true
+  },
   mixins: [datePickerMixin],
   data: {
     setIndex: [] as number[] // picker 每次打开时，根据该字段选择值。点击确定按钮后，setIndex 更新
