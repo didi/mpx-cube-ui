646a21933f8d5fa457e7ab433d5e89537252387f
QiuShuiBai <1125097005@qq.com>
2025-02-10 16:45:21 +0800

    fix: 替换.ts文件名

--- packages/mpx-cube-ui/src/components/switch/switch.ts
+++ packages/mpx-cube-ui/src/components/switch/switch.ts
@@ -2,10 +2,18 @@ import { createComponent } from '../../common/helper/create-component'
 
 createComponent({
   properties: {
+    /**
+     * @description 开关状态，可直接赋值
+     * @optional true/false
+     */
     value: {
       type: Boolean,
       value: false
     },
+    /**
+     * @description 是否禁用
+     * @optional true/false
+     */
     disabled: {
       type: Boolean,
       value: false
@@ -25,7 +33,7 @@ createComponent({
   },
   watch: {
     value: {
-      handler (newVal) {
+      handler(newVal) {
         this.isOn = newVal
       },
       immediate: true
@@ -36,7 +44,9 @@ createComponent({
       if (this.disabled) return
       const newValue = !this.isOn
       this.isOn = newValue
+      // 当开关状态变化时触发
       this.triggerEvent('change', { value: newValue })
+      // 当开关状态变化时触发
       this.triggerEvent('input', { value: newValue })
     }
   }
