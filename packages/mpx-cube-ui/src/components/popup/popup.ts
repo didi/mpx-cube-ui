583e3037ca198f5581ce2ff361cbe786590d5653
QiuShuiBai <1125097005@qq.com>
2025-02-16 21:34:45 +0800

    refactor(popup): rn代码抽离mixin

--- packages/mpx-cube-ui/src/components/popup/popup.ts
+++ packages/mpx-cube-ui/src/components/popup/popup.ts
@@ -1,27 +1,19 @@
-import mpx, { createComponent, REACTHOOKSEXEC } from '@mpxjs/core'
-import { useEffect } from 'react'
+import { createComponent } from '@mpxjs/core'
 import {
   popupMixin,
   visibilityMixin,
   themePropsMixin,
   varContextMixin
 } from '../../common/mixins'
-
+import rnMixin from './rn-mixin'
 const EVENT_MASK_CLICK = 'maskClick'
-
-type ANIMATION_PRESET = Record<
-  string,
-  (
-    animationOptions: WechatMiniprogram.AnimationOption,
-  ) => void
->
 
 createComponent({
   options: {
     multipleSlots: true,
     styleIsolation: 'shared'
   },
-  mixins: [popupMixin, visibilityMixin, themePropsMixin, varContextMixin],
+  mixins: [popupMixin, visibilityMixin, themePropsMixin, varContextMixin, rnMixin],
   properties: {
     // 拓展 class 属性，cube-${type}，可用于样式覆盖和定制
     type: {
@@ -67,83 +59,7 @@ createComponent({
   data: {
     visibleClass: '',
     transitionClass: '',
-    animationData: {} as WechatMiniprogram.AnimationExportResult,
-    rootAnimationData: {} as WechatMiniprogram.AnimationExportResult,
-    maskAnimationData: {} as WechatMiniprogram.AnimationExportResult,
-    ANIMATION_PRESET: {} as ANIMATION_PRESET,
     display: false,
-    contentRect: {} as WechatMiniprogram.BoundingClientRectCallbackResult
-  },
-  [REACTHOOKSEXEC] () {
-    useEffect(() => {
-      if (this.visibleClass === 'show') {
-        this.isVisible = true
-      } else if (this.visibleClass === 'hide') {
-        this.isVisible = false
-      }
-    })
-  },
-  lifetimes: {
-    created() {
-      // eslint-disable-next-line
-      // @ts-ignore
-      if (__mpx_mode__ === 'ios' || __mpx_mode__ === 'android') {
-        this.ANIMATION_PRESET = {
-          'cube-popup_mask_fade_transition': (animationOptions) => {
-            const animation = this.maskAnimation || (this.maskAnimation = mpx.createAnimation({ ...animationOptions, timingFunction: 'ease-out' }))
-            if (this.isVisible) {
-              animation.opacity(0.4).step()
-            } else {
-              animation.opacity(0).step()
-            }
-            this.maskAnimationData = animation.export()
-          },
-          'cube-popup_transition': (animationOptions) => {
-            if (!this.isVisible) {
-              setTimeout(() => {
-                this.display = false
-              }, animationOptions.duration)
-            }
-          },
-          'move-up': (animationOptions) => {
-            const hasTranslate = !!this.animation
-            const animation = this.animation || (this.animation = mpx.createAnimation(animationOptions))
-            if (this.isVisible) {
-              if (!hasTranslate) {
-                animation.translateY(this.contentInfo.height).step({ duration: 0 })
-              }
-              animation.translateY(0).step()
-            } else {
-              animation.translateY(this.contentInfo.height).step()
-            }
-            this.animationData = animation.export()
-          },
-          'move-right': () => { /* empty fn */ },
-          'move-left': () => { /* empty fn */ },
-          'move-down': () => { /* empty fn */ },
-          fade: (animationOption) => {
-            const animation = this.animation || (this.animation = mpx.createAnimation(animationOption))
-            if (this.isVisible) {
-              animation.opacity(1).step()
-            } else {
-              animation.opacity(0).step()
-            }
-            this.animationData = animation.export()
-          }
-        }
-        this.$watch(
-          'isVisible',
-          (n, o) => {
-            if (!!n === !!o) return
-            this.rnAnimation({
-              duration: 300,
-              timingFunction: 'ease-out'
-            })
-          },
-          { immediate: true }
-        )
-      }
-    }
   },
   computed: {
     rootClass() {
@@ -166,44 +82,9 @@ createComponent({
         cls[`cube-popup-${this.display ? 'show' : 'hide'}`] = true
       }
       return cls
-    },
-    contentInfo() {
-      return {
-        height: this.styleConfig?.content?.height || this.contentRect.height
-      }
     }
   },
   methods: {
-    initContentRect() {
-      if (this.isMaskClosed) {
-        this.isMaskClosed = false
-        return
-      }
-      if (this.styleConfig?.content?.height) return
-      return new Promise((resolve) => {
-        this.$nextTick(() => {
-          this.$refs['popup-content'].boundingClientRect((res) => {
-            this.contentRect = res
-            resolve(res)
-          }).exec()
-        })
-      })
-    },
-    getWindowInfo() {
-      if (this.windowInfo) return this.windowInfo
-      return (this.windowInfo = mpx.getWindowInfo())
-    },
-    async rnAnimation(
-      animationOptions: WechatMiniprogram.StepOption = {}
-    ) {
-      await this.initContentRect()
-      const names = [...Object.keys(this.rootClass).filter(v => this.rootClass[v]), this.transitionClass]
-      if (!names.length) return
-      names.forEach(v => {
-        const presetFn = this.ANIMATION_PRESET[v]
-        presetFn && presetFn(animationOptions)
-      })
-    },
     contentHideAnimationend() {
       // bug:
       // a 页面弹窗，进入到b页面，再b页面返回a页面，会有 hide 的animation 动画
---
+++ packages/mpx-cube-ui/src/components/popup/rn-mixin.ts
@@ -0,0 +1,126 @@
+import mpx, { getMixin, REACTHOOKSEXEC } from '@mpxjs/core'
+import { useEffect } from 'react'
+
+let mixin = {} as Parameters<typeof getMixin>[0]
+// eslint-disable-next-line
+// @ts-ignore
+if (__mpx_mode__ === 'ios' && __mpx_mode__ === 'android') {
+  type ANIMATION_PRESET = Record<
+    string,
+    (
+      animationOptions: WechatMiniprogram.AnimationOption,
+    ) => void
+  >
+  mixin = {
+    [REACTHOOKSEXEC] () {
+      useEffect(() => {
+        if (this.visibleClass === 'show') {
+          this.isVisible = true
+        } else if (this.visibleClass === 'hide') {
+          this.isVisible = false
+        }
+      })
+    },
+    data: {
+      animationData: {} as WechatMiniprogram.AnimationExportResult,
+      rootAnimationData: {} as WechatMiniprogram.AnimationExportResult,
+      maskAnimationData: {} as WechatMiniprogram.AnimationExportResult,
+      ANIMATION_PRESET: {} as ANIMATION_PRESET,
+      contentRect: {} as WechatMiniprogram.BoundingClientRectCallbackResult
+    },
+    computed: {
+      contentInfo() {
+        return {
+          height: this.styleConfig?.content?.height || this.contentRect.height
+        }
+      }
+    },
+    lifetimes: {
+      created() {
+        this.ANIMATION_PRESET = {
+          'cube-popup_mask_fade_transition': (animationOptions) => {
+            const animation = this.maskAnimation || (this.maskAnimation = mpx.createAnimation({ ...animationOptions, timingFunction: 'ease-out' }))
+            if (this.isVisible) {
+              animation.opacity(0.4).step()
+            } else {
+              animation.opacity(0).step()
+            }
+            this.maskAnimationData = animation.export()
+          },
+          'cube-popup_transition': (animationOptions) => {
+            if (!this.isVisible) {
+              setTimeout(() => {
+                this.display = false
+              }, animationOptions.duration)
+            }
+          },
+          'move-up': (animationOptions) => {
+            const hasTranslate = !!this.animation
+            const animation = this.animation || (this.animation = mpx.createAnimation(animationOptions))
+            if (this.isVisible) {
+              if (!hasTranslate) {
+                animation.translateY(this.contentInfo.height).step({ duration: 0 })
+              }
+              animation.translateY(0).step()
+            } else {
+              animation.translateY(this.contentInfo.height).step()
+            }
+            this.animationData = animation.export()
+          },
+          'move-right': () => { /* empty fn */ },
+          'move-left': () => { /* empty fn */ },
+          'move-down': () => { /* empty fn */ },
+          fade: (animationOption) => {
+            const animation = this.animation || (this.animation = mpx.createAnimation(animationOption))
+            if (this.isVisible) {
+              animation.opacity(1).step()
+            } else {
+              animation.opacity(0).step()
+            }
+            this.animationData = animation.export()
+          }
+        }
+        this.$watch(
+          'isVisible',
+          (n, o) => {
+            if (!!n === !!o) return
+            this.rnAnimation({
+              duration: 300,
+              timingFunction: 'ease-out'
+            })
+          },
+          { immediate: true }
+        )
+      }
+    },
+    methods: {
+      initContentRect() {
+        if (this.isMaskClosed) {
+          this.isMaskClosed = false
+          return
+        }
+        if (this.styleConfig?.content?.height) return
+        return new Promise((resolve) => {
+          this.$nextTick(() => {
+            this.$refs['popup-content'].boundingClientRect((res) => {
+              this.contentRect = res
+              resolve(res)
+            }).exec()
+          })
+        })
+      },
+      async rnAnimation(
+        animationOptions: WechatMiniprogram.StepOption = {}
+      ) {
+        await this.initContentRect()
+        const names = [...Object.keys(this.rootClass).filter(v => this.rootClass[v]), this.transitionClass]
+        if (!names.length) return
+        names.forEach(v => {
+          const presetFn = this.ANIMATION_PRESET[v]
+          presetFn && presetFn(animationOptions)
+        })
+      }
+    }
+  }
+}
+export default getMixin(mixin)
