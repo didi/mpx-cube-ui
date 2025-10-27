92167d156e92f075066887909a81d6d4a3a2a503
QiuShuiBai <1125097005@qq.com>
2025-10-27 20:18:26 +0800

    feat: rn demo 环境配置完毕

--- example/ReactNativeProject/metro.config.js
+++ example/ReactNativeProject/metro.config.js
@@ -5,7 +5,7 @@ const { mergeConfig } = require('@react-native/metro-config');
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {};
 
