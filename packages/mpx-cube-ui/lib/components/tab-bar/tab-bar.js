add6a1a3fc831aec4bdca1f5db12bf480613d5db
WeilinerL <2207284210@qq.com>
2025-11-26 22:19:22 +0800

    chore: build

--- packages/mpx-cube-ui/lib/components/tab-bar/tab-bar.js
+++ packages/mpx-cube-ui/lib/components/tab-bar/tab-bar.js
@@ -1,11 +1,16 @@
-import mpx, { createComponent, MOUNTED, UNMOUNTED, ONRESIZE } from '@mpxjs/core';
-import useObserver from '@mpxjs/mpx-cube-ui/src/common/helper/useObserver';
+import mpx, { createComponent, MOUNTED, UNMOUNTED } from '@mpxjs/core';
+import Tab from './tab.mpx?resolve';
 const EVENT_INPUT = 'input';
 const EVENT_CHANGE = 'change';
 const EVENT_CLICK = 'click';
 createComponent({
     options: {
         styleIsolation: 'shared'
+    },
+    relations: {
+        [Tab]: {
+            type: 'child'
+        }
     },
     properties: {
         /**
@@ -16,7 +21,10 @@ createComponent({
             optionalTypes: [String],
             value: 0
         },
-        data: {
+        /**
+         * @description 用于 cube-tab-bar 渲染的数据，当需要使用内置的默认插槽，此参数必传，数组的每一项是一个 Object 类型，包括 label、icon 和 value，如果使用自定义插槽，也需要传递此值
+         */
+        tabs: {
             type: Array,
             value: []
         },
@@ -43,23 +51,32 @@ createComponent({
         useTransition: {
             type: Boolean,
             value: true
+        },
+        /**
+         * @description 是否需要自定义默认插槽
+         * @optional true/false
+         */
+        customizeContent: {
+            type: Boolean,
+            value: false
         }
     },
     data: {
-        inited: false,
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        unobserver: () => { },
-        els: []
+        useTransitionCopy: false,
+        // 下划线开头的属性不会变成响应式
+        _resizeTimer: null,
+        els: [],
+        cubeTabBar: true // 提供 web 侧查找父组件标识
     },
     computed: {
         tabBarClass() {
             return {
                 'cube-tab-bar_inline': this.inline,
-                'cube-tab-bar_transition': this.useTransition
+                'cube-tab-bar_transition': this.useTransitionCopy
             };
         },
         currentIndex() {
-            return this.data.findIndex(item => item.value === this.value);
+            return this.tabs.findIndex(item => item.value === this.value || item.label === this.value);
         },
         currentOffset() {
             let offsetLeft = 0;
@@ -77,46 +94,67 @@ createComponent({
         }
     },
     methods: {
-        change(item) {
-            this.triggerEvent(EVENT_CLICK, {
-                value: item.value
-            });
-            if (item.value === this.value) {
+        trigger(value) {
+            this.triggerEvent(EVENT_CLICK, { value });
+            if (value === this.value) {
                 return;
             }
             const changedEvents = [EVENT_INPUT, EVENT_CHANGE];
             changedEvents.forEach((eventType) => {
-                this.triggerEvent(eventType, {
-                    value: item.value
-                });
+                this.triggerEvent(eventType, { value });
             });
             this.$nextTick(() => {
                 this.calcAllTabWidth();
             });
         },
         calcAllTabWidth() {
-            mpx.createSelectorQuery().in(this).selectAll('.tab-bar-item').fields({
+            // 使用了自定义插槽渲染tab-bar
+            const children = this.getRelationNodes(Tab);
+            if (children?.length) {
+                this.els = [];
+                children.forEach(child => {
+                    mpx.createSelectorQuery().in(child).select('.cube-tab').fields({
+                        size: true,
+                        computedStyle: ['margin-left', 'margin-right']
+                    }, (rect) => {
+                        this.els.push(rect);
+                    }).exec();
+                });
+                return;
+            }
+            // 没有使用自定义插槽
+            mpx.createSelectorQuery().in(this).selectAll('.cube-tab-bar > .cube-tab').fields({
                 size: true,
                 computedStyle: ['margin-left', 'margin-right']
             }, (rects) => {
                 this.els = rects;
             }).exec();
+        },
+        _resizeHandler() {
+            clearTimeout(this._resizeTimer);
+            this._resizeTimer = setTimeout(() => {
+                this.calcAllTabWidth();
+            }, 60);
         }
     },
     [MOUNTED]() {
-        this.unobserver = useObserver.call(this, '.tab-bar', () => {
-            if (!this.inited) {
-                this.calcAllTabWidth();
-            }
-            this.inited = true;
-        });
-    },
-    [ONRESIZE]() {
-        this.$nextTick(() => {
-            this.calcAllTabWidth();
-        });
+        // 避免用户一开始就看到下划线滚动到目标位置
+        const initialUseTransition = this.useTransition;
+        this.calcAllTabWidth();
+        if (initialUseTransition) {
+            this.useTransitionCopy = false;
+            setTimeout(() => {
+                this.useTransitionCopy = initialUseTransition;
+            }, 300);
+        }
+        if (__mpx_mode__ === 'web') {
+            window.addEventListener('resize', this._resizeHandler);
+        }
     },
     [UNMOUNTED]() {
-        this.unobserver?.();
+        clearTimeout(this._resizeTimer);
+        if (__mpx_mode__ === 'web') {
+            window.removeEventListener('resize', this._resizeHandler);
+        }
     }
 });
--- packages/mpx-cube-ui/lib/components/tab-bar/tab.js
+++ packages/mpx-cube-ui/lib/components/tab-bar/tab.js
@@ -1,5 +1,11 @@
-import { createComponent } from '@mpxjs/core';
+import { createComponent, MOUNTED } from '@mpxjs/core';
+import TabBar from './index.mpx?resolve';
 createComponent({
+    relations: {
+        [TabBar]: {
+            type: 'parent'
+        }
+    },
     options: {
         multipleSlots: true,
         styleIsolation: 'shared'
@@ -16,6 +22,64 @@ createComponent({
         icon: {
             type: String,
             value: ''
+        },
+        /**
+         * @description 是否需要自定义插槽（默认内容）
+         * @optional true/false
+         */
+        customizeContent: {
+            type: Boolean,
+            value: false
+        },
+        /**
+         * @description 是否需要自定义插槽（icon）
+         * @optional true/false
+         */
+        customizeIcon: {
+            type: Boolean,
+            value: false
+        }
+    },
+    data: {
+        parent: undefined
+    },
+    [MOUNTED]() {
+        this.parent = this.getParent();
+    },
+    computed: {
+        isActive() {
+            if (!this.parent) {
+                return;
+            }
+            return this.parent.value === this.value;
+        }
+    },
+    methods: {
+        getParent() {
+            if (__mpx_mode__ === 'web') {
+                const parent = this.$parent;
+                if (parent.$data.cubeTabBar) {
+                    return parent;
+                }
+                return null;
+            }
+            else {
+                let parent = this.selectOwnerComponent();
+                if (parent?.cubeTabBar) {
+                    return parent;
+                }
+                parent = this.getRelationNodes(TabBar);
+                if (Array.isArray(parent)) {
+                    return parent[0];
+                }
+                return parent;
+            }
+        },
+        handleClick() {
+            if (!this.parent) {
+                return;
+            }
+            this.parent.trigger(this.value);
         }
     }
 });
