500ea4d0146e39275f1eeaf913f1a37c9219826d
QiuShuiBai <1125097005@qq.com>
2026-03-20 17:42:41 +0800

    chore: 1.4.13-slider.1

---
+++ packages/mpx-cube-ui/lib/components/slider/gesture.js
@@ -0,0 +1,30 @@
+import { View } from 'react-native';
+import { runOnJS } from 'react-native-reanimated';
+import { createElement, useMemo, Children } from 'react';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+const Slider = ({ ontouchstart, ontouchmove, ontonouchend, children }) => {
+    const panGesture = useMemo(() => {
+        return Gesture.Pan()
+            .enabled(true) // 通过手势启用状态控制是否可拖拽
+            .onBegin(() => {
+            'worklet';
+            runOnJS(ontouchstart)();
+        })
+            .onUpdate((event) => {
+            'worklet';
+            runOnJS(ontouchmove)({
+                touches: [{
+                        clientX: event.absoluteX
+                    }]
+            });
+        })
+            .onEnd(() => {
+            'worklet';
+            runOnJS(ontonouchend)();
+        });
+    });
+    return createElement(View, {}, createElement(GestureDetector, {
+        gesture: panGesture
+    }, createElement(View, {}, ...Children.toArray(children))));
+};
+export default Slider;
